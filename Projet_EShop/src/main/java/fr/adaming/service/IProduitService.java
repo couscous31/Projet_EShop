9831e2e7a9f61a2d71dcec43f18008c23a3dc981
inti0327 <inti0327@DESKTOP-6FRA8CT>
2018-04-09 16:15:46 +0200

    ced : service MB produit

--- Projet_EShop/src/main/java/fr/adaming/service/IProduitService.java
+++ Projet_EShop/src/main/java/fr/adaming/service/IProduitService.java
@@ -1,5 +1,24 @@
 package fr.adaming.service;
 
+import java.util.List;
+
+import fr.adaming.model.Categorie;
+import fr.adaming.model.Produit;
+
 public interface IProduitService {
+	
+public List<Produit> getAllProduit();
+	
+	public Produit addProduit (Produit pr);
+	
+	public int updateProduit (Produit pr);
+	
+	 public int deleteProduit (Produit pr);
+	 
+	public Produit getProduitById (Produit pr); 
+	
+	public List<Produit> produitParCategorie (Produit pr, Categorie cat);
+	
+	public List<Produit> getParMotCle(String motCle);
 
 }
