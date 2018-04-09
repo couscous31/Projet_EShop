e7bf6b48bdf39b5c53429cdff43a162d8a1bda24
inti0333 <inti0333@DESKTOP-B6HP6U1>
2018-04-09 17:15:18 +0200

    mat : clientMB

--- Projet_EShop/src/main/java/fr/adaming/managedBean/ClientMB.java
+++ Projet_EShop/src/main/java/fr/adaming/managedBean/ClientMB.java
@@ -3,6 +3,8 @@ package fr.adaming.managedBean;
 import java.io.Serializable;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.RequestScoped;
@@ -13,31 +15,36 @@ import javax.servlet.http.HttpSession;
 import fr.adaming.model.Client;
 import fr.adaming.service.IClientService;
 
-@ManagedBean(name="clMB")
+@ManagedBean(name = "clMB")
 @RequestScoped
 
-public class ClientMB implements Serializable{
-	
-	//Transfo assos avec IClientService
-	@ManagedProperty(value="#{clientService}")
+public class ClientMB implements Serializable {
+
+	// Transfo assos avec IClientService
+	@ManagedProperty(value = "#{clientService}")
 	private IClientService clService;
 
 	public void setClService(IClientService clService) {
 		this.clService = clService;
 	}
-	
-	//Attributs du MB
+
+	// Attributs du MB
 	private Client client;
 	private List<Client> listeClients;
 	HttpSession maSession;
 
-	//Constructeur vide
+	// Constructeur vide
 	public ClientMB() {
-		this.client=new Client();
+		this.client = new Client();
 	}
 	
+	//Méthode init
+	@PostConstruct
+	public void init(){
+		maSession = (HttpSession) FacesContext.getCurrentInstance().getExternalContext().getSession(false);
+	}
 
-	//Getter et setter
+	// Getter et setter
 	public Client getClient() {
 		return client;
 	}
@@ -53,13 +60,33 @@ public class ClientMB implements Serializable{
 	public void setListeClients(List<Client> listeClients) {
 		this.listeClients = listeClients;
 	}
+
+	// Méthode du MB
+
+	public void modifClient() {
+		// appel de la méthode
+		int verif = clService.updateClient(client);
+
+		if (verif != 0) {
+			Client clModif = clService.isExist(client);
+			maSession.setAttribute("clientModif", clModif);
+		} else {
+			FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("La modification a échoué !"));
+		}
+	}
+
+	public void supClient() {
+		// appel de la méthode
+		int verif = clService.deleteClient(client);
+
+		if (verif != 0) {
+			FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("La suppression est un succès !"));
+		} else {
+			FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("La suppression a échoué !"));
+		}
+	}
 	
-	
-	//Méthode du MB
-	
-	public void modifClient(){
-		//appel de la méthode
-		int verif=clService.updateClient(client);
+	public void seConnecter(){
 		
 	}
 
