7c8caec0be28e942adfd99a17cee38b90b1dea0e
inti0333 <inti0333@DESKTOP-B6HP6U1>
2018-04-09 14:24:33 +0200

    mat : model

--- Projet_EShop/src/main/java/fr/adaming/model/Commande.java
+++ Projet_EShop/src/main/java/fr/adaming/model/Commande.java
@@ -1,5 +1,103 @@
 package fr.adaming.model;
 
-public class Commande {
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+@Table(name = "commandes")
+
+public class Commande implements Serializable {
+
+	// Attributs
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_com")
+	private long id;
+	@Temporal(TemporalType.DATE)
+	private Date date;
+
+	// Transfo assos avec ligne de commande
+	@OneToMany(mappedBy = "commande")
+	private List<LigneCommande> listeCl;
+
+	// Transfo assos avec client
+	@ManyToOne
+	@JoinColumn(name = "cl_id", referencedColumnName = "id_cl")
+	private Client client;
+
+	// Transfo assos avec agent
+	@ManyToOne
+	@JoinColumn(name = "admin_id", referencedColumnName = "id_admin")
+	private Administrateur admin;
+
+	// Constructeurs
+	public Commande() {
+		super();
+	}
+
+	public Commande(Date date) {
+		super();
+		this.date = date;
+	}
+
+	public Commande(long id, Date date) {
+		super();
+		this.id = id;
+		this.date = date;
+	}
+
+	// Getter et setter
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public List<LigneCommande> getListeCl() {
+		return listeCl;
+	}
+
+	public void setListeCl(List<LigneCommande> listeCl) {
+		this.listeCl = listeCl;
+	}
+
+	public Client getClient() {
+		return client;
+	}
+
+	public void setClient(Client client) {
+		this.client = client;
+	}
+
+	public Administrateur getAdmin() {
+		return admin;
+	}
+
+	public void setAdmin(Administrateur admin) {
+		this.admin = admin;
+	}
 
 }
--- Projet_EShop/src/main/java/fr/adaming/model/LigneCommande.java
+++ Projet_EShop/src/main/java/fr/adaming/model/LigneCommande.java
@@ -12,76 +12,85 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="lignes_commandes")
+@Table(name = "lignes_commandes")
 
-public class LigneCommande implements Serializable{
-	
-	//Attributs
+public class LigneCommande implements Serializable {
+
+	// Attributs
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
-	@Column(name="id_lc")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_lc")
 	private long id;
 	private long quantite;
 	private double prix;
-	
-	//Transfo assos avec produit
+
+	// Transfo assos avec produit
 	@ManyToOne
-	@JoinColumn(name="p_id", referencedColumnName="id_p")
+	@JoinColumn(name = "p_id", referencedColumnName = "id_p")
 	private Produit produit;
-	
-	//Transfo assos avec commande
+
+	// Transfo assos avec commande
 	@ManyToOne
-	@JoinColumn(name="com_id", referencedColumnName="id_com")
+	@JoinColumn(name = "com_id", referencedColumnName = "id_com")
 	private Commande commande;
-	
-	//Constructeurs
+
+	// Constructeurs
 	public LigneCommande() {
 		super();
 	}
+
 	public LigneCommande(long quantite, double prix) {
 		super();
 		this.quantite = quantite;
 		this.prix = prix;
 	}
+
 	public LigneCommande(long id, long quantite, double prix) {
 		super();
 		this.id = id;
 		this.quantite = quantite;
 		this.prix = prix;
 	}
-	
-	//Getter et setter
+
+	// Getter et setter
 	public long getId() {
 		return id;
 	}
+
 	public void setId(long id) {
 		this.id = id;
 	}
+
 	public long getQuantite() {
 		return quantite;
 	}
+
 	public void setQuantite(long quantite) {
 		this.quantite = quantite;
 	}
+
 	public double getPrix() {
 		return prix;
 	}
+
 	public void setPrix(double prix) {
 		this.prix = prix;
 	}
+
 	public Produit getProduit() {
 		return produit;
 	}
+
 	public void setProduit(Produit produit) {
 		this.produit = produit;
 	}
+
 	public Commande getCommande() {
 		return commande;
 	}
+
 	public void setCommande(Commande commande) {
 		this.commande = commande;
 	}
-	
-	
 
 }
