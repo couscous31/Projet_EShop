3e7139594663edbe3bdb457e9da07ffcbbbe45fd
inti0333 <inti0333@DESKTOP-B6HP6U1>
2018-04-10 16:07:15 +0200

    mat : panier

--- Projet_EShop/src/main/java/fr/adaming/model/Panier.java
+++ Projet_EShop/src/main/java/fr/adaming/model/Panier.java
@@ -1,5 +1,45 @@
 package fr.adaming.model;
 
-public class Panier {
+import java.io.Serializable;
+import java.util.List;
+
+public class Panier implements Serializable{
+	
+	//Attributs
+	private int idP;
+	
+	//Transfo assos avec ligne de commande
+	private List<LigneCommande> listeLc;
+
+	//Constructeurs
+	public Panier() {
+		super();
+	}
+
+	public Panier(int idP) {
+		super();
+		this.idP = idP;
+	}
+
+	//Getter et setter
+	public int getIdP() {
+		return idP;
+	}
+
+	public void setIdP(int idP) {
+		this.idP = idP;
+	}
+
+	public List<LigneCommande> getListeLc() {
+		return listeLc;
+	}
+
+	public void setListeLc(List<LigneCommande> listeLc) {
+		this.listeLc = listeLc;
+	}
+	
+	
+	
+	
 
 }
